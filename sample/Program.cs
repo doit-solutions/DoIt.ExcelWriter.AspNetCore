3d1e8950bfc68e0fd3642c988589bdd3b745df74
David Nordvall <david.nordvall@doitsolutions.se>
2023-03-07 15:36:39 +0000

    Removed commented code in the sample project.

--- sample/Program.cs
+++ sample/Program.cs
@@ -1,13 +1,6 @@
-using DoIt.ExcelWriter.AspNetCore.Sample;
-
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
-// app.UseRouting();
-// app.UseEndpoints(routes =>
-// {
-//     routes.MapFallbackToController(nameof(DefaultController.Get), nameof(DefaultController).Replace("Controller", string.Empty));
-// });
 app.UseHttpsRedirection();
 app.UseHsts();
 app.MapControllers();
